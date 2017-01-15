d0a2bd81c865b4020190eedb5b4e3b60629070fb
Travis CI <travis@travis-ci.org>
2017-01-15 21:25:37 +0800

    Deploy Github pages only if on master

--- push.sh
+++ push.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
+fi
+
 rev=$(git rev-parse --short HEAD)
 
 cd "$TRAVIS_BUILD_DIR"
