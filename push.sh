2647467deda6644f8f03bd680cdbd7953fed73c4
Ka Yi <chua.kayi@yahoo.com.sg>
2017-01-15 15:55:54 +0800

    Reconfigure travis

--- push.sh
+++ push.sh
@@ -1,21 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-}
+set -o errexit -o nounset
 
-commit_website_files() {
-  git checkout -b gh-pages
-  git add . *.html
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
+fi
 
-upload_files() {
-  git remote add origin https://${GH_TOKEN}@github.com/kychua/addressbook-level4.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin gh-pages
-}
+rev=$(git rev-parse --short HEAD)
 
-setup_git
-commit_website_files
-upload_files
+cd build/docs/html5
+
+git init
+git config user.name "Travis CI"
+git config user.email "travis@travis-ci.org"
+
+git remote add upstream https://${GH_TOKEN}@github.com/kychua/addressbook-level4.git > /dev/null 2>&1
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "Rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
