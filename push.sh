b4e8fe3813c543b76b6ba0ba2eca39c09d271108
Travis CI <travis@travis-ci.org>
2017-01-15 20:18:00 +0800

    Change Travis to use HTTPS API Token

--- push.sh
+++ push.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
 
-set -o errexit -o nounset
-
-if [ "$TRAVIS_BRANCH" != "master" ]
-then
-  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
-  exit 0
-fi
-
-rev=$(git rev-parse --short HEAD)
-
+cd "$TRAVIS_BUILD_DIR"
 cd build/docs/html5
 
 git init
-git config user.name "Travis CI"
-git config user.email "travis@travis-ci.org"
-
-git remote add upstream https://${GH_TOKEN}@github.com/kychua/addressbook-level4.git > /dev/null 2>&1
-git fetch upstream
-git reset upstream/gh-pages
-
-touch .
-
-git add -A .
-git commit -m "Rebuild pages at ${rev}"
-git push -q upstream HEAD:gh-pages
+git checkout -b gh-pages
+git add .
+git -c user.name='travis' -c user.email='travis' commit -m init
+git push --force --quiet "https://${GITHUB_API_KEY}@$github.com/kychua/addressbook-level4.git" master:gh-pages > /dev/null 2>&1
+cd $TRAVIS_BUILD_DIR
